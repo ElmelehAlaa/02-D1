a643aac5ffd25658b61b654c2ca7f837d79a7e2f
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-07-03 17:56:00 +0200

    U2L1

--- D1.js
+++ D1.js
@@ -7,17 +7,6 @@ REGOLE
 - Dovrai creare una direttiva di output per testare le variabili od i risultati delle espressioni che crei
 */
 
-
-
-
-
-
-
-
-
-
-
-
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
@@ -42,93 +31,48 @@ null quando in una variabile non viene definito intenzionalmente un valore.
 
 */
 
- /*ESERCIZIO 2
+/*ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
  */
 
-
- /*let name ="Alaa";
-console.log("name"); 
-
-
-
-
-
-
-
-
+let name2 = "Alaa";
+console.log(name2);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/*let numero=12;
-let numero1=20;
-console.log('numero+numero1')
-
-
-
-
-
-
-
-
+let numero0 = 12;
+let numero1 = 20;
+console.log(numero0 + numero1);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/*let x = 12
-
-
-
-
-
-
-
-
-
+let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
+name2 = "Elmeleh";
+/*const capelli = "neri";
+capelli = "biondi";
 
- /*
- let name= "Elmeleh" 
-const capelli = "neri"
-capelli= "biondi"
+console.log(capelli);*/
 
-console.log(capelli)
-
-let capelli="neri"
-capelli="biondi"
-console.log(capelli)
-
-
-
-
-
-
-
-  
-
+let capelli = "neri";
+capelli = "biondi";
+console.log(capelli);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/*let numero = 4
-console.log(numero-x)
-
-
-
-
-
-
-
-
+let numero = 4;
+console.log(numero - x);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -136,11 +80,10 @@ console.log(numero-x)
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/*let name1="john"
-let name2="John"
+let name3 = "john";
+let name4 = "John";
 
-console.log(name1===name2)*/
-
+console.log(name3 === name4);
 
 /*let name1="john"
 let name2="john"
